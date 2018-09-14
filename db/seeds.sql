0fc1fe155d556d806039deef54b7d444c4110980
Hayley Sweet <mrs.kornilenko@gmail.com>
2018-09-14 00:25:05 -0400

    initial files + stucture

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('Smoked Gouda & Bacon Cheeseburger', false);
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('California Sun Avocado Burger', false);
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('Breakfast Burger', false);
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('Chicken Burger with Garlic Aioli', true);
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('Ranch-it-up Chick-urger', true);
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('Memphis Blacken Fish Burger', true);
